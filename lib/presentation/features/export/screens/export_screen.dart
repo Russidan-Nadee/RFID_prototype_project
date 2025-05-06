7945ef21da239d45474cb4ad8cf88fd2314cdcde
Russidan-Nadee <russidan.nadee@gmail.com>
2025-05-06 10:31:33 +0700

    feature/export-screen-navbar

--- lib/presentation/features/export/screens/export_screen.dart
+++ lib/presentation/features/export/screens/export_screen.dart
@@ -1,8 +1,12 @@
+// lib/presentation/features/export/screens/export_screen.dart
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../../common_widgets/layouts/screen_container.dart';
 import '../../../common_widgets/buttons/primary_button.dart';
+import '../../../common_widgets/layouts/app_bottom_navigation.dart';
 import '../blocs/export_bloc.dart';
+import '../../../../core/navigation/navigation_service.dart';
+import '../../../features/main/blocs/navigation_bloc.dart';
 
 class ExportScreen extends StatefulWidget {
   const ExportScreen({Key? key}) : super(key: key);
@@ -23,10 +27,30 @@ class _ExportScreenState extends State<ExportScreen> {
     'Last Scan',
   ];
 
+  void _onNavigationTap(int index) {
+    // ดึง NavigationBloc จาก Provider
+    final navigationBloc = Provider.of<NavigationBloc>(context, listen: false);
+
+    // อัปเดต index ใน NavigationBloc
+    navigationBloc.setCurrentIndex(index);
+
+    // ใช้ NavigationService เพื่อไปยังหน้าที่ต้องการ
+    NavigationService.navigateToTabByIndex(context, index);
+  }
+
   @override
   Widget build(BuildContext context) {
+    // เพิ่มการอ่านค่า currentIndex จาก NavigationBloc
+    final navigationBloc = Provider.of<NavigationBloc>(context);
+    final currentIndex = navigationBloc.currentIndex;
+
     return ScreenContainer(
       appBar: AppBar(title: const Text('Export Assets Data'), elevation: 0),
+      // เพิ่ม bottomNavigationBar เข้าไป
+      bottomNavigationBar: AppBottomNavigation(
+        currentIndex: currentIndex,
+        onTap: _onNavigationTap,
+      ),
       child: Consumer<ExportBloc>(
         builder: (context, bloc, child) {
           return Column(
